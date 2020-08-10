4daaa89f2552dd5fc737f3cd37b685e42a25a59d
omar abdi <o.hassanabdi@se18.qmul.ac.uk>
2020-08-10 16:16:55 +0100

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Loading from "./components/app/Loading";
 
 const Nav = lazy(() => import("./components/app/Nav"));
 const Home = lazy(() => import("./pages/Home"));
-const Portfolio = lazy(() => import("./pages/Portfolio"));
+const Projects = lazy(() => import("./pages/projects"));
 const Contact = lazy(() => import("./pages/Contact"));
 
 function App() {
@@ -16,8 +16,8 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/portfolio">
-            <Portfolio />
+          <Route path="/projects">
+            <Projects />
           </Route>
           <Route path="/contact">
             <Contact />
